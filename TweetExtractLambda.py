0b8d0e9c32f4b46f3fae243fbb1f3c1066b0b215
Fiona Sequeira <fiona.sequeira891@gmail.com>
2017-12-04 13:49:58 -0500

    Update TweetExtractLambda.py

--- TweetExtractLambda.py
+++ TweetExtractLambda.py
@@ -19,7 +19,7 @@ sqs = boto3.client(
     'sqs',
     aws_access_key_id = '',
     aws_secret_access_key = '',
-    region_name = 'us-west-2'
+    region_name = ''
 )
 
 def lambda_handler(event, context):
